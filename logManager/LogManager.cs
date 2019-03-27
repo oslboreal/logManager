98e7eef289fb917d055d9a065ad59b6a13bebcd8
Vallejo Juan Marcos <oslboreal@gmail.com>
2019-03-27 16:21:53 -0300

    First aproach of LogManager

---
+++ logManager/LogManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logManager
+{
+    public class LogManager
+    {
+        private static LogManager _instance;
+        public static LogManager Intance
+        {
+            get
+            {
+                return LogManager._instance;
+            }
+        }
+        public List<string> Logs
+        {
+            get
+            {
+                return _strings;
+            }
+        }
+
+        public delegate void logAddedEventRaiser(List<string> logs);
+        public event logAddedEventRaiser OnLogAdded;
+        private List<string> _strings;
+
+        static LogManager()
+        {
+            LogManager._instance = new LogManager();
+        }
+
+        public LogManager()
+        {
+            this._strings = new List<string>();
+        }
+
+        public void Log(string text)
+        {
+            _strings.Add(text);
+            OnLogAdded(_strings);
+        }
+
+        public void SaveLogsAs(string dir)
+        {
+            if (!File.Exists(dir))
+                File.Create(dir).Close();
+
+            using (StreamWriter file = new StreamWriter(dir, false))
+                file.Write(dir);
+        }
+    }
+}
